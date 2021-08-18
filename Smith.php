b61267daad64ed196299265e5f002dd93d574815
sam <sammy@hexmakina.be>
2021-08-18 23:43:40 +0200

    PSR-12, phpcbf autofix

--- Smith.php
+++ Smith.php
@@ -4,109 +4,119 @@ namespace HexMakina\Smith;
 
 class Smith
 {
-  const REPORTING_USER = 'user_messages';
-  const INDEX_FILTER = 'filter';
-  const INDEX_OPERATOR = 'operator';
+    const REPORTING_USER = 'user_messages';
+    const INDEX_FILTER = 'filter';
+    const INDEX_OPERATOR = 'operator';
 
   // IS-54-16 : Behold, I have created the smith who blows the fire of coals
   // $options : https://www.php.net/manual/fr/session.configuration.php
-  public function __construct($options)
-  {
-    if(isset($options['session_name']))
+    public function __construct($options)
     {
-      session_name($options['session_name']);
-      unset($options['session_name']);
-    }
+        if (isset($options['session_name'])) {
+            session_name($options['session_name']);
+            unset($options['session_name']);
+        }
 
-    session_start($options); // https://www.php.net/manual/fr/function.session-start.php
-  }
+        session_start($options); // https://www.php.net/manual/fr/function.session-start.php
+    }
 
 
   // IS-54-16 : and produces a weapon for its purpose
-  public function add_message($level, $message, $context=[])
-  {
-    if(!isset($_SESSION[self::REPORTING_USER]))
-      $_SESSION[self::REPORTING_USER] = [];
+    public function add_message($level, $message, $context = [])
+    {
+        if (!isset($_SESSION[self::REPORTING_USER])) {
+            $_SESSION[self::REPORTING_USER] = [];
+        }
 
-    if(!isset($_SESSION[self::REPORTING_USER][$level]))
-      $_SESSION[self::REPORTING_USER][$level] = [];
+        if (!isset($_SESSION[self::REPORTING_USER][$level])) {
+            $_SESSION[self::REPORTING_USER][$level] = [];
+        }
 
-    $_SESSION[self::REPORTING_USER][$level][] = [$message, $context];
-  }
+        $_SESSION[self::REPORTING_USER][$level][] = [$message, $context];
+    }
 
-  public function messages($level=null)
-  {
-    if(is_null($level))
-      return $_SESSION[self::REPORTING_USER];
+    public function messages($level = null)
+    {
+        if (is_null($level)) {
+            return $_SESSION[self::REPORTING_USER];
+        }
 
-    return $_SESSION[self::REPORTING_USER][$level] ?? null;
-  }
+        return $_SESSION[self::REPORTING_USER][$level] ?? null;
+    }
 
-  public function reset_messages($level=null)
-  {
-    $this->reset(self::REPORTING_USER, $level);
-  }
+    public function reset_messages($level = null)
+    {
+        $this->reset(self::REPORTING_USER, $level);
+    }
 
 
-  public function add_runtime_filters($filters)
-  {
-    $_SESSION[self::INDEX_FILTER] = array_merge($_SESSION[self::INDEX_FILTER] ?? [], $filters);
-  }
+    public function add_runtime_filters($filters)
+    {
+        $_SESSION[self::INDEX_FILTER] = array_merge($_SESSION[self::INDEX_FILTER] ?? [], $filters);
+    }
 
-  public function has_filter($filter_name) : bool
-  {
-    return isset($_SESSION[self::INDEX_FILTER][$filter_name]) && strlen(''.$_SESSION[self::INDEX_FILTER][$filter_name]) > 0;
-  }
+    public function has_filter($filter_name): bool
+    {
+        return isset($_SESSION[self::INDEX_FILTER][$filter_name]) && strlen('' . $_SESSION[self::INDEX_FILTER][$filter_name]) > 0;
+    }
 
-  public function filters($filter_name=null, $value=null)
-  {
-    if(is_null($filter_name))
-      return $_SESSION[self::INDEX_FILTER];
+    public function filters($filter_name = null, $value = null)
+    {
+        if (is_null($filter_name)) {
+            return $_SESSION[self::INDEX_FILTER];
+        }
 
-    if(!is_null($value))
-      $_SESSION[self::INDEX_FILTER][$filter_name] = $value;
+        if (!is_null($value)) {
+            $_SESSION[self::INDEX_FILTER][$filter_name] = $value;
+        }
 
-    return $_SESSION[self::INDEX_FILTER][$filter_name] ?? null;
-  }
+        return $_SESSION[self::INDEX_FILTER][$filter_name] ?? null;
+    }
 
-  public function reset_filters($filter_name=null)
-  {
-    $this->reset(self::INDEX_FILTER, $filter_name);
-  }
+    public function reset_filters($filter_name = null)
+    {
+        $this->reset(self::INDEX_FILTER, $filter_name);
+    }
 
-  public function operator_id($setter = null)
-  {
-    if(!is_null($setter))
-      $_SESSION[self::INDEX_OPERATOR] = ['id' => $setter, 'set_on' => time()];
+    public function operator_id($setter = null)
+    {
+        if (!is_null($setter)) {
+            $_SESSION[self::INDEX_OPERATOR] = ['id' => $setter, 'set_on' => time()];
+        }
 
-    return $_SESSION[self::INDEX_OPERATOR]['id'] ?? null;
-  }
+        return $_SESSION[self::INDEX_OPERATOR]['id'] ?? null;
+    }
 
-  public function operator_started_on()
-  {
-    return $_SESSION[self::INDEX_OPERATOR]['set_on'] ?? null;
-  }
+    public function operator_started_on()
+    {
+        return $_SESSION[self::INDEX_OPERATOR]['set_on'] ?? null;
+    }
 
   // IS-54-16 : I have also created the ravager to destroy
-  public function destroy() : bool
-  {
-
-    if(ini_get("session.use_cookies"))
+    public function destroy(): bool
     {
-      $params = session_get_cookie_params();
-      setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-      );
-    }
-    return session_destroy();
-  }
 
-  private function reset($index, $part=null)
-  {
-    if(is_null($part))
-      $_SESSION[$index]=[];
-    else
-      unset($_SESSION[$index][$part]);
-  }
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(
+                session_name(),
+                '',
+                time() - 42000,
+                $params["path"],
+                $params["domain"],
+                $params["secure"],
+                $params["httponly"]
+            );
+        }
+        return session_destroy();
+    }
+
+    private function reset($index, $part = null)
+    {
+        if (is_null($part)) {
+            $_SESSION[$index] = [];
+        } else {
+            unset($_SESSION[$index][$part]);
+        }
+    }
 }
