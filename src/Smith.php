88b700191bab735e1ebef9b588aeb7b7ffc4ba43
HexMakina <20891495+HexMakina@users.noreply.github.com>
2022-11-15 14:20:10 +0100

    added typing cleanup PHPStan 9 PSR12

--- src/Smith.php
+++ src/Smith.php
@@ -11,7 +11,7 @@ class Smith implements StateAgentInterface
 
     // IS-54-16 : Behold, I have created the smith who blows the fire of coals
     // $options : https://www.php.net/manual/fr/session.configuration.php
-    public static function getInstance($options = []): Smith
+    public static function getInstance(array $options = []): StateAgentInterface
     {
         if (is_null(self::$instance)) {
             self::$instance = new Smith($options);
@@ -20,17 +20,7 @@ class Smith implements StateAgentInterface
         return self::$instance;
     }
 
-    private static function sessionsAreDisabled(): bool
-    {
-        return session_status() === PHP_SESSION_DISABLED;
-    }
-
-    private static function hasNoSession(): bool
-    {
-        return session_status() === PHP_SESSION_NONE;
-    }
-
-    private function __construct($options = [])
+    private function __construct(array $options = [])
     {
         if (self::sessionsAreDisabled()) {
             throw new \UnexpectedValueException(__CLASS__ . '::PHP_SESSION_DISABLED');
@@ -42,34 +32,26 @@ class Smith implements StateAgentInterface
             session_start($options); // https://www.php.net/manual/fr/function.session-start.php
         }
 
-        if (!isset($_SESSION[self::INDEX_MESSAGES])) {
-            $_SESSION[self::INDEX_MESSAGES] = [];
-        }
-
-        if (!isset($_SESSION[self::INDEX_FILTER])) {
-            $_SESSION[self::INDEX_FILTER] = [];
-        }
-
-        if (!isset($_SESSION[self::INDEX_OPERATOR])) {
-            $_SESSION[self::INDEX_OPERATOR] = [];
+        foreach ([self::INDEX_MESSAGES, self::INDEX_FILTER, self::INDEX_OPERATOR] as $k) {
+            $_SESSION[$k] ??= [];
         }
     }
 
     // camelCase wrapper for setcookie, coherent with getCookie
     public function setCookie(
-        $name,
-        $value = "",
-        $expires_in = 365 * 24 * 60 * 60,
-        $path = "/",
-        $domain = "",
-        $secure = false,
-        $httponly = false
+        string $name,
+        string $value = "",
+        int $expires_in = 365 * 24 * 60 * 60,
+        string $path = "/",
+        string $domain = "",
+        bool $secure = false,
+        bool $httponly = false
     ): bool {
         return setcookie($name, $value, time() + $expires_in, $path, $domain, $secure, $httponly);
     }
 
     // returns the value stored or null
-    public function getCookie($name)
+    public function getCookie(string $name): mixed
     {
         return $_COOKIE[$name] ?? null;
     }
@@ -78,7 +60,7 @@ class Smith implements StateAgentInterface
 
 
     // IS-54-16 : and produces a weapon for its purpose
-    public function addMessage($level, $message, $context = []): void
+    public function addMessage(mixed $level, string $message, array $context = []): void
     {
         if (!isset($_SESSION[self::INDEX_MESSAGES][$level])) {
             $_SESSION[self::INDEX_MESSAGES][$level] = [];
@@ -87,7 +69,7 @@ class Smith implements StateAgentInterface
         $_SESSION[self::INDEX_MESSAGES][$level][] = [$message, $context];
     }
 
-    public function messages($level = null)
+    public function messages(mixed $level = null)
     {
         if (is_null($level)) {
             return $_SESSION[self::INDEX_MESSAGES];
@@ -96,7 +78,7 @@ class Smith implements StateAgentInterface
         return $_SESSION[self::INDEX_MESSAGES][$level] ?? null;
     }
 
-    public function resetMessages($level = null): void
+    public function resetMessages(mixed $level = null): void
     {
         $this->reset(self::INDEX_MESSAGES, $level);
     }
@@ -104,23 +86,23 @@ class Smith implements StateAgentInterface
 
 
 
-    public function addRuntimeFilters($filters): void
+    public function addRuntimeFilters(array $filters): void
     {
         $_SESSION[self::INDEX_FILTER] = array_merge($_SESSION[self::INDEX_FILTER], $filters);
     }
 
-    public function hasFilter($filter_name): bool
+    public function hasFilter(string $filter_name): bool
     {
         return isset($_SESSION[self::INDEX_FILTER][$filter_name])
         && strlen('' . $_SESSION[self::INDEX_FILTER][$filter_name]) > 0;
     }
 
-    public function addFilter($filter_name, $value): void
+    public function addFilter(string $filter_name, mixed $value): void
     {
         $_SESSION[self::INDEX_FILTER][$filter_name] = $value;
     }
 
-    public function filters($filter_name = null, $value = null)
+    public function filters(string $filter_name = null, mixed $value = null): mixed
     {
         if (is_null($filter_name)) {
             return $_SESSION[self::INDEX_FILTER];
@@ -133,13 +115,13 @@ class Smith implements StateAgentInterface
         return $_SESSION[self::INDEX_FILTER][$filter_name] ?? null;
     }
 
-    public function resetFilters($filter_name = null): void
+    public function resetFilters(string $filter_name = null): void
     {
         $this->reset(self::INDEX_FILTER, $filter_name);
     }
 
 
-    public function operatorId($setter = null)
+    public function operatorId(mixed $setter = null): mixed
     {
         if (!is_null($setter)) {
             $_SESSION[self::INDEX_OPERATOR] = ['id' => $setter, 'set_on' => time()];
@@ -148,7 +130,7 @@ class Smith implements StateAgentInterface
         return $_SESSION[self::INDEX_OPERATOR]['id'] ?? null;
     }
 
-    public function operatorCheckinDate()
+    public function operatorCheckinDate(): mixed
     {
         return $_SESSION[self::INDEX_OPERATOR]['set_on'] ?? null;
     }
@@ -173,12 +155,23 @@ class Smith implements StateAgentInterface
         return session_destroy();
     }
 
-    private function reset($index, $part = null): void
+    private function reset(mixed $index, mixed $part = null): void
     {
         if (is_null($part)) {
             $_SESSION[$index] = [];
         } else {
             unset($_SESSION[$index][$part]);
         }
+    }
+
+
+    private static function sessionsAreDisabled(): bool
+    {
+        return session_status() === PHP_SESSION_DISABLED;
+    }
+
+    private static function hasNoSession(): bool
+    {
+        return session_status() === PHP_SESSION_NONE;
     }
 }
