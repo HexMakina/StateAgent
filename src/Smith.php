09b71179281551ad4ad50d565e0eddb3ad751150
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-03-09 20:49:19 +0100

    Add session management methods to Smith class

--- src/Smith.php
+++ src/Smith.php
@@ -15,9 +15,34 @@ class Smith implements StateAgentInterface
     {
         if (is_null(self::$instance)) {
             self::$instance = new Smith($options);
+           
         }
 
         return self::$instance;
+    }
+
+    public function start(): string
+    {
+        if (self::hasNoSession()) {
+            session_start();
+        }
+
+        return session_name();
+    }
+
+    public function set(string $key, $value): void
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    public function has(string $key): bool
+    {
+        return isset($_SESSION[$key]);
+    }
+
+    public function get(string $key)
+    {
+        return $_SESSION[$key] ?? null;
     }
 
     private function __construct(array $options = [])
@@ -32,9 +57,6 @@ class Smith implements StateAgentInterface
             session_start($options); // https://www.php.net/manual/fr/function.session-start.php
         }
 
-        foreach ([self::INDEX_MESSAGES, self::INDEX_FILTER, self::INDEX_OPERATOR] as $k) {
-            $_SESSION[$k] ??= [];
-        }
     }
 
     // camelCase wrapper for setcookie, coherent with getCookie
@@ -56,84 +78,6 @@ class Smith implements StateAgentInterface
         return $_COOKIE[$name] ?? null;
     }
 
-
-
-
-    // IS-54-16 : and produces a weapon for its purpose
-    public function addMessage(string $level, string $message, array $context = []): void
-    {
-        if (!isset($_SESSION[self::INDEX_MESSAGES][$level])) {
-            $_SESSION[self::INDEX_MESSAGES][$level] = [];
-        }
-
-        $_SESSION[self::INDEX_MESSAGES][$level][] = [$message, $context];
-    }
-
-    public function messages(string $level = null)
-    {
-        if (is_null($level)) {
-            return $_SESSION[self::INDEX_MESSAGES];
-        }
-
-        return $_SESSION[self::INDEX_MESSAGES][$level] ?? null;
-    }
-
-    public function resetMessages(string $level = null): void
-    {
-        $this->reset(self::INDEX_MESSAGES, $level);
-    }
-
-
-
-
-    public function addRuntimeFilters(array $filters): void
-    {
-        $_SESSION[self::INDEX_FILTER] = array_merge($_SESSION[self::INDEX_FILTER], $filters);
-    }
-
-    public function hasFilter(string $filter_name): bool
-    {
-        return isset($_SESSION[self::INDEX_FILTER][$filter_name])
-        && strlen('' . $_SESSION[self::INDEX_FILTER][$filter_name]) > 0;
-    }
-
-    public function addFilter(string $filter_name, string $value): void
-    {
-        $_SESSION[self::INDEX_FILTER][$filter_name] = $value;
-    }
-
-    public function filters(string $filter_name = null, string $value = null): ?string
-    {
-        if (is_null($filter_name)) {
-            return $_SESSION[self::INDEX_FILTER];
-        }
-
-        if (!is_null($value)) {
-            $this->addFilter($filter_name, $value);
-        }
-
-        return $_SESSION[self::INDEX_FILTER][$filter_name] ?? null;
-    }
-
-    public function resetFilters(string $filter_name = null): void
-    {
-        $this->reset(self::INDEX_FILTER, $filter_name);
-    }
-
-    public function operatorId(string $setter = null): ?string
-    {
-        if ($setter !== null) {
-            $_SESSION[self::INDEX_OPERATOR] = ['id' => $setter, 'set_on' => time()];
-        }
-
-        return $_SESSION[self::INDEX_OPERATOR]['id'] ?? null;
-    }
-
-    public function operatorCheckinDate(): string
-    {
-        return $_SESSION[self::INDEX_OPERATOR]['set_on'] ?? null;
-    }
-
     // IS-54-16 : I have also created the ravager to destroy
     public function destroy(): bool
     {
@@ -152,15 +96,6 @@ class Smith implements StateAgentInterface
         }
 
         return session_destroy();
-    }
-
-    private function reset(string $index, string $part = null): void
-    {
-        if (is_null($part)) {
-            $_SESSION[$index] = [];
-        } else {
-            unset($_SESSION[$index][$part]);
-        }
     }
 
 
